5afc8998df8ebead8776bae365ea5b63f153ef6d
flasky06 <bonnienjuguna106@gmail.com>
2024-05-08 14:40:17 +0300

    left sidebar, mobile navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,32 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstNAme: "Bonface", lastNAme: "Njuguna" };
+
   return (
-    <main>
-      sidbar
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image
+            src="/icons/logo.svg"
+            width={30}
+            height={30}
+            className="size-[24px] max-xl:size-[14px]"
+            alt="menu"
+          />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
